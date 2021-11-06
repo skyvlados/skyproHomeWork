cae52e958cac7b4daa63933f1cfc6294ebd4fd9e
skyvlados <vladislav.repin@skyeng.ru>
2021-11-06 22:46:03 +0300

    Homework1.5

---
+++ src/pro/sky/java/course1/lesson1_5/Homework.java
@@ -0,0 +1,69 @@
+package pro.sky.java.course1.lesson1_5;
+
+import java.util.Arrays;
+
+public class Homework {
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        String a = Arrays.toString(arr);
+
+        task1(arr);
+        task2(arr);
+        task3(arr);
+        task4();
+    }
+
+    private static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+
+    }
+
+    private static void task1(int[] arr) {
+        System.out.println("Первое задание");
+        int allSumma = 0;
+        for (int i = 0; i < arr.length; i++) {
+            allSumma = allSumma + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + allSumma + " рублей.");
+    }
+
+    private static void task2(int[] arr) {
+        System.out.println("Второе задание");
+        int maxValue = 0;
+        int minValue = 200000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minValue + " рублей. Максимальная сумма трат за день составила " + maxValue + " рублей.");
+    }
+
+    private static void task3(int[] arr) {
+        System.out.println("Третее задание");
+        float allSumma = 0;
+        for (int i = 0; i < arr.length; i++) {
+            allSumma = allSumma + arr[i];
+        }
+        float middleSumma = allSumma / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + middleSumma + " рублей.");
+    }
+
+    private static void task4() {
+        System.out.println("Четвертое задание");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[reverseFullName.length - 1 - i]);
+        }
+    }
+
+
+}
